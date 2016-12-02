20b05ff7717eaae266bcc1eb9167fc6f5fc567b3
Hotckiss <kirilenko_aA@mail.ru>
2016-12-02 23:36:38 +0300

    Update scheme.cpp

--- figure/src/scheme.cpp
+++ figure/src/scheme.cpp
@@ -16,23 +16,28 @@ void Scheme::push_back_figure(Figure* fg) {
     this->figures_[size] = fg;
     (this->size)++;
 }
-void Scheme::remove_figure(int id) {
+int Scheme::find_figure_index_by_id(int id) {
     for (int i = 0; i < size; i++)
-        if (figures_[i]->get_id() == id) {
-            delete figures_[i];
-            for (int j = i; j < size - 1; j++)
-                figures_[j] = figures_[j + 1];
-            size--;
-        }
+        if (figures_[i]->get_id() == id)
+            return i;
+    return -1;
+}
+void Scheme::remove_figure(int id) {
+    int ind = find_figure_index_by_id(id);
+    if (ind == -1) return;
+    delete figures_[ind];
+    for (int j = ind; j < size - 1; j++)
+        figures_[j] = figures_[j + 1];
+    size--;
 }
 void Scheme::print_all_figures() {
     for (int i = 0; i < size; i++)
         figures_[i]->print();
 }
 void Scheme::zoom_figure(int id, int factor) {
-    for (int i = 0; i < size; i++)
-        if(figures_[i]->get_id() == id)
-            figures_[i]->zoom(factor);
+    int ind = find_figure_index_by_id(id);
+    if (ind == -1) return;
+    figures_[ind]->zoom(factor);
 }
 Figure* Scheme::is_inside_figure(int x, int y) {
     for (int i = 0; i < size; i++)
@@ -41,7 +46,7 @@ Figure* Scheme::is_inside_figure(int x, int y) {
     return NULL;
 }
 void Scheme::move(int id, int new_x, int new_y) {
-    for (int i = 0; i < size; i++)
-        if (figures_[i]->get_id() == id)
-            figures_[i]->move(new_x, new_y);
+    int ind = find_figure_index_by_id(id);
+    if (ind == -1) return;
+    figures_[ind]->move(new_x, new_y);
 }
