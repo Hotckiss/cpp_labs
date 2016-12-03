67b0adf7e4b0874441c79ca30c68c82cf461a4c7
Hotckiss <kirilenko_aA@mail.ru>
2016-12-03 22:57:46 +0300

    Update rectangle.cpp

--- figure/src/rectangle.cpp
+++ figure/src/rectangle.cpp
@@ -6,7 +6,7 @@ void Rectangle::print() const {
     printf("Rectangle %i: x = %i y = %i width = %i height = %i\n", id, x, y, w, h);
 }
 bool Rectangle::is_inside(int x, int y) const {
-    return (x <= this->x + w / 2 && x >= this->x - w / 2 && y <= this->y + h / 2 && y >= this->y - h / 2);
+    return (2 * x <= 2 * this->x + w && 2 * x >= 2 * this->x - w && 2 * y <= 2 * this->y + h && 2 * y >= 2 * this->y - h);
 }
 void Rectangle::zoom(int factor) {
     w *= factor;
